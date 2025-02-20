c5c16395eaee0782da927829cec522b51da1be8d
Ramesh Bojanapu <84425972+lovelyram0143@users.noreply.github.com>
2025-02-20 15:35:56 +0530

    fix: update user creation to use event data and comment out required fields in User schema

--- src/data/models/User.ts
+++ src/data/models/User.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
+    // required: true,
     unique: true,
   },
   firstName: {
@@ -18,14 +18,14 @@ const UserSchema = new mongoose.Schema({
   },
   clerkId: {
     type: String,
-    required: true,
+    // required: true,
     unique: true,
   },
   email: { type: String, required: true, unique: true },
-  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  isOnline: { type: Boolean, default: false },
-  lastSeen: { type: Date, default: null },
+  // friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  // friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  // isOnline: { type: Boolean, default: false },
+  // lastSeen: { type: Date, default: null },
 });
 
 export default mongoose.models.User || mongoose.model('User', UserSchema);
